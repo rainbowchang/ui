fefe9665f7dcf256e4d84cdee242142cf8e9e67d
bleech243 <15601586070@163.com>
2019-12-14 20:11:28 +0800

    点击事件获取客户信息

--- src/apis/restUtils.js
+++ src/apis/restUtils.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 // 使用axios，自定义get/post方法
 
+
+axios.defaults.withCredentials=true;
 export const post = (url, param, consumer) => {
     return axios.post(appConfig.restUrl(url), param,
     {
@@ -16,7 +18,7 @@ export const post = (url, param, consumer) => {
 }
 
 export const get = (url, consumer) => {
-    return axios.post(appConfig.restUrl(url),
+    return axios.get(appConfig.restUrl(url),
     {
         headers: {
             'Content-Type': 'application/json;charset=UTF-8'
@@ -26,35 +28,3 @@ export const get = (url, consumer) => {
         consumer(response);
     });
 }
-// export default{
-//     data(){
-//         return {
-//             resp: []
-//     }},
-
-//     methods: {
-//         post(url, param){
-//         axios.post(appConfig.restUrl(url), param,
-//         {
-//             headers: {
-//             'Content-Type': 'application/json;charset=UTF-8'
-//         }})
-//         .then(response => {
-//                 console.log(response);
-//                 this.resp = response;
-//             }).catch(function (response) {
-//                 alert(response);
-//             });
-//         },
-
-//         get(url){
-//             axios.get(appConfig.restUrl(url))
-//             .then(response => {
-//                     console.log(response);
-//                     this.resp = response;
-//                 }).catch(function (response) {
-//                     alert(response)
-//             });
-//         }
-//     }
-// }
--- src/components/meualist/equipstatus.vue
+++ src/components/meualist/equipstatus.vue
@@ -29,7 +29,7 @@
       <!-- 工厂表单 -->
       <factory v-show="factory"></factory>
       <!-- 客户表单 -->
-      <statustable v-show="statustable"></statustable>
+      <statustable v-show="statustable" ref="statustable"></statustable>
     </div>
   </div>
 </template>
@@ -37,6 +37,7 @@
 import { VueTreeList, Tree, TreeNode } from "vue-tree-list";
 import statustable from "./statustable";
 import factory from "./factory";
+import {get} from "@/apis/restUtils";
 export default {
   components: {
     VueTreeList,
@@ -353,6 +354,37 @@ export default {
 
       // }
       this.treeParam = params;
+      get("/organization/customer/getFirstCustomer", reponse => {
+        var content = this.$refs.statustable.content;
+        reponse.data.forEach(element => {
+          var machine = element.machineBean;
+          var machineStatus = element.machineStatus;
+          var contentElement = {};
+          contentElement.name = machine.description;
+          contentElement.model = machine.model;
+          contentElement.serial = machine.sn;
+          contentElement.CNC = machine.plcType;
+          contentElement.productionDate = machine.dateOfProduction
+          contentElement.warning = "";
+          machineStatus.alarms.forEach(element =>{
+            contentElement.warning =  contentElement.warning + "告警码: " + element + " ";
+        });
+        contentElement.status = "unknown";
+        if(contentElement.warning != ""){
+          contentElement.status = "故障";  
+        }else{
+          switch(machineStatus.WorkState){
+            case 0:
+              contentElement.status = "加工";
+              break;
+            case 1:
+              contentElement.status = "停机";
+              break;
+          }
+        }   
+        content.push(contentElement); 
+        });
+      });  
     },
 
     addNode() {
