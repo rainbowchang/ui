a3e15316554a4021f70603385315a278fee226ee
tianwenli@elextec.com <tianwenli@elextec.com>
2019-12-29 23:06:31 +0800

    begin to add the customer

--- src/components/meualist/customerInfoModal.vue
+++ src/components/meualist/customerInfoModal.vue
@@ -4,7 +4,7 @@
 
     <div>
         <span>节点信息：</span>
-        <i-select :model.sync="nodeType" style="width:200px" @on-change = "nodeTypeSelectCallback">
+        <i-select v-model="nodeType" style="width:200px" @on-change = "nodeTypeSelectCallback">
             <i-option value="customer">客户信息</i-option>
             <i-option value = "plc">机器信息</i-option>
             <i-option value = "other">自定义</i-option>
@@ -13,21 +13,22 @@
 
     <div v-if= "customerVisible">
         <span>客户列表：</span>
-        <i-select :model.sync="customerName" style="width:200px" @on-change = "customerSelectCallback">
+        <i-select v-model="customerName" style="width:200px" @on-change = "customerSelectCallback">
             <i-option v-for="item in customerList" :key = "item.value" :value="item.label">{{ item.label }}</i-option>
         </i-select> 
     </div>
     <br>
     <div v-if= "plcVisible">
         <span>机器信息列表：</span>
-        <i-select :model.sync="plcSn" style="width:200px" @on-change = "plcSelectCallback">
+        <i-select v-model="plcSn" style="width:200px" @on-change = "plcSelectCallback">
             <i-option v-for="item in plcList" :key = "item.value" :value="item.label">{{ item.label }}</i-option>
         </i-select> 
     </div>
     <br>
     <div>
         <span>别名：</span>
-        <i-input :value.sync="inputAlias" placeholder="请输入..." style="width: 300px" @on-blur = "inputCallback"></i-input>
+     <!--    <i-input :value.sync="inputAlias" placeholder="请输入..." style="width: 300px" @on-change = "inputCallback"></i-input> -->
+         <Input v-model="inputAlias" placeholder="Enter something..." style="width: 300px" @on-blur = "inputCallback"/>
     </div>
   </div>
 </template>
@@ -99,20 +100,21 @@
         return "";
      },
      customerSelectCallback(value){
-        this.customerName = value;
+        // this.customerName = value;
         this.nodeType = "CUSTOMER";
         this.$emit('showInfo', value, this.inputAlias, "CUSTOMER");
      },
      plcSelectCallback(value){
-        this.plcSn = value;
+        // this.plcSn = value;
         this.nodeType = "LEAF";
         this.$emit('showInfo', value,this.inputAlias, "LEAF");
      },
      inputCallback(alias){
-        this.inputAlias = alias;
+        // this.inputAlias = alias;
+        alert(" alias:" + JSON.stringify(this.inputAlias) + " plcsn:" + this.plcSn
+           + " customer:" + this.customerName);
         switch(this.nodeType){
           case "CUSTOMER":
-             alert(" node is :" + this.customerName + " alias:" + alias + " type:" +  this.nodeType);
              this.$emit('showInfo', this.customerName, alias, this.nodeType);
              break;
           case "LEAF":
