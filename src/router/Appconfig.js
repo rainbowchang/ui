0af9544695b3276287947e0b2fe14ddf3b09d6d5
bleech243 <15601586070@163.com>
2020-02-14 23:31:33 +0800

    Need to set environment VUE_APP_BACK_END_URL,default http://127.0.0.1:8000

--- src/router/Appconfig.js
+++ src/router/Appconfig.js
@@ -1,5 +1,10 @@
+var backEndUrl = "http://127.0.0.1:8000"
+if(process.env.VUE_APP_BACK_END_URL != undefined){
+    backEndUrl = process.env.VUE_APP_BACK_END_URL;
+}
+
 var AppConfig = {
-    host:"http://127.0.0.1:8000",
+    host: backEndUrl,
     restUrl: function(paraUrl){
         return this.host + paraUrl;
     }
