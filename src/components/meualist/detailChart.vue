68d7f997d8c3acd4c34b0c4cfad850d52b860eec
zhangyouwen <1013910434@qq.com>
2020-06-29 14:51:20 +0800

    TJ

--- src/components/meualist/detailChart.vue
+++ src/components/meualist/detailChart.vue
@@ -178,7 +178,7 @@ export default {
       if(val === null || val === undefined){
         return;
       }
-      if(val.currentProgram!==null){val.currentProgram = (val.currentProgram).substring(0,(val.currentProgram).indexOf('_'))}
+      if(val.currentProgram!==null && (val.currentProgram).indexOf('_')!==-1){val.currentProgram = (val.currentProgram).substring(0,(val.currentProgram).indexOf('_'))}
       this.tableList = val.table;
       this.sn = val.sn;
       this.workPieces = val.workPieces;
