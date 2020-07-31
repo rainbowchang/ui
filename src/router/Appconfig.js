a5768e982ef66a4c826677ba1fdb27e958a0fdd4
zhangyaxu <zhangyaxu@elextec.com>
2020-07-31 11:34:50 +0800

    fix https

--- src/router/Appconfig.js
+++ src/router/Appconfig.js
@@ -1,4 +1,6 @@
-var backEndUrl = "http://127.0.0.1:8000"
+//var backEndUrl = "http://127.0.0.1:8000"
+var backEndUrl = `${window.location.protocol}//${window.location.host}`
+
 if(process.env.VUE_APP_BACK_END_URL != undefined){
     backEndUrl = process.env.VUE_APP_BACK_END_URL;
 }
@@ -6,7 +8,7 @@ if(process.env.VUE_APP_BACK_END_URL != undefined){
 var AppConfig = {
     host: backEndUrl,
     restUrl: function(paraUrl){
-        return this.host + paraUrl;
+        return this.host + '/api' + paraUrl;
     }
 }
 
