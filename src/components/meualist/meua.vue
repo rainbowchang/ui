6a280b971f37a61c199fdddde155e09a45247b1a
rainbowchang <rainbow_chang@hotmail.com>
2023-03-26 12:05:08 +0800

    字符调整

--- src/components/meualist/meua.vue
+++ src/components/meualist/meua.vue
@@ -9,7 +9,7 @@
             style="background:#595959;"
         >
             <div class="logo">
-                <span>YUANKONG</span>
+                <span>宁庆</span>
                 <span style="font-size:4px">®</span>
             </div>
             <MenuItem name="/index" :class="index?'red':''">首页</MenuItem>
