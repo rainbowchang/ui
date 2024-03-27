8468f5cab2aae2f76268a422cd9bb51c969b7eaa
rainbowchang <rainbow_chang@hotmail.com>
2024-03-27 17:21:55 +0800

    中途提交

---
+++ src/components/meualist/report.vue
@@ -0,0 +1,31 @@
+<template>
+    <Form :model="formItem" :label-width="80">
+        <FormItem label="日期">
+            <DatePicker type="date" placeholder="选择日期" v-model="formItem.selectDate"></DatePicker>
+        </FormItem>
+    </Form>
+</template>
+
+<script>
+export default {
+    name: "report",
+    data() {
+        return {
+            formItem: {
+                selectDate: '',
+            },
+        }
+    },
+    props: ['date'],
+    mounted: function () {
+        this.formItem = this.date;
+    },
+
+}
+</script>
+
+<style scoped>
+.wrapper {
+
+}
+</style>
